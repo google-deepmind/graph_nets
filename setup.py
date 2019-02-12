530aeadbc686f436e907ca8d111b5a72788906ee
alvarosg <alvarosg@google.com>
2019-02-12 17:02:42 +0000

    Bump version to 1.0.4.dev

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ Tensorflow and Sonnet.
 
 setup(
     name="graph_nets",
-    version="1.0.3",
+    version="1.0.4.dev",
     description="Library for building graph networks in Tensorflow and Sonnet.",
     long_description=description,
     author="DeepMind",
